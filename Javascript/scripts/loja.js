95d4ff224aadb17cf2f0853635e39d53b5114360
louis0113 <luizhenrique011304@gmail.com>
2024-11-26 12:03:07 -0300

    Delete Javascript directory

--- Javascript/scripts/loja.js
+++
@@ -1,9 +0,0 @@
-const preco = Number(prompt("Digite o preço do produto: "));
-const parcela = Number(prompt("Digite o número da parcela: "));
-
-const avista = preco - (preco/10);
-const parcelado = preco / parcela; 
-
-alert("Preço R$: " + preco.toFixed(2) + "\nÀ Vista: " + avista.toFixed(2) + "\nOu " + parcela + "x de  R$: " + parcelado.toFixed(2));
-
-console.log("Preço R$: " + preco.toFixed(2) + "\nÀ Vista: " + avista.toFixed(2) + "\nOu " + parcela + "x de  R$: " + parcelado.toFixed(2));
--- Javascript/scripts/pizza.js
+++
@@ -1,9 +0,0 @@
-const conta = Number(prompt("Digite o quanto deu sua conta: "));
-
-const pessoas = Number(prompt("Digite quantas pessoas vão pagar: "));
-
-const divi = conta / pessoas;
-
-alert(("O Valor da Conta R$: " + conta.toFixed(2) + "\nNúmero de Clientes: " + pessoas + "\nValor por ClientesR$: " + divi.toFixed(2)));
-
-console.log("O Valor da Conta R$: " + conta.toFixed(2) + "\nNúmero de Clientes: " + pessoas + "\nValor por Clientes R$: " + divi.toFixed(2));
